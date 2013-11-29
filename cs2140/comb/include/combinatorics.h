b429099ed006e4202738e9156aa25641559c1184
Mark Gondree <gondree@users.noreply.github.com>
2013-11-29 18:28:26 +0000

    Added better examples for using bell_errno, lucas_errno. Added an inline function for checking multiplication overflows

--- cs2140/comb/include/combinatorics.h
+++ cs2140/comb/include/combinatorics.h
@@ -6,11 +6,15 @@
 #ifndef _COMBINATORICS_H_
 #define _COMBINATORICS_H_
 
+#include <limits.h>
+
 //
 // type for holding large combinatorial values
 //
-typedef long long int comb_t;
+typedef long int comb_t;
 
+#define COMB_MAX  LONG_MAX
+#define COMB_MIN  LONG_MIN
 
 //
 // identifiers for supported combinatorial sequences
